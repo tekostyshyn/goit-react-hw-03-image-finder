929045a40e6c678c7a4473310d4d5f7db44e6427
tekostyshyn <tekostyshyn@gmail.com>
2023-04-23 23:00:17 +0300

    http

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import Seachbar from './Searchbar';
 import ImageGallery from './ImageGallery';
+import Button from './Button';
 import axios from 'axios';
 
 const KEY = '8210264-2ea871c1a05460bb4aaa242b8';
@@ -16,33 +17,54 @@ export class App extends Component {
   async componentDidUpdate(prevProps, prevState) {
     const { pageNumber, searchQuery } = this.state;
 
-    if (prevState.searchQuery !== searchQuery || prevState.pageNumber !== pageNumber) {
+    if (
+      prevState.searchQuery !== searchQuery ||
+      prevState.pageNumber !== pageNumber
+    ) {
       const response = await axios.get(
         `?q=${searchQuery}&page=${pageNumber}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
       );
-      const seachedPictures = response.data.hits.map(
+      const newPictures = response.data.hits.map(
         ({ id, webformatURL, largeImageURL }) => {
           return { id, webformatURL, largeImageURL };
         }
       );
-      this.setState({ pictures: seachedPictures });
+
+      if (
+        prevState.searchQuery !== searchQuery &&
+        prevState.pageNumber !== pageNumber
+      ) {
+        const searchedPictures = [...newPictures];
+        this.setState({ pictures: searchedPictures });
+        return;
+      }
+
+      const searchedPictures = [...prevState.pictures, ...newPictures];
+      this.setState({ pictures: searchedPictures });
     }
   }
 
   getSearchQuery = value => {
     this.setState({
       searchQuery: value,
+      pageNumber: 1,
+      pictures: [],
     });
   };
 
-  search = () => {};
+  changePageNumber = () => {
+    this.setState({
+      pageNumber: this.state.pageNumber + 1,
+    });
+  };
 
   render() {
-    console.log(this.state.pictures);
+    const {pictures} = this.state;
     return (
       <div>
         <Seachbar onSubmit={this.getSearchQuery} />
-        <ImageGallery pics={this.state.pictures} />
+        <ImageGallery pics={pictures} />
+        {pictures.length > 0 && <Button onClick={this.changePageNumber} />}
       </div>
     );
   }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ({onClick}) => {
+    return <button className="Button" type="button" onClick={onClick}>Load more</button>
+}
+
+export default Button
