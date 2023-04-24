bd1d23a2babac912a90a87fa62e103dc16aace1d
tekostyshyn <tekostyshyn@gmail.com>
2023-04-24 11:53:21 +0300

    final

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,13 +1,34 @@
 import ImageGalleryItem from 'components/ImageGalleryItem';
+import { Component } from 'react';
 
-const ImageGallery = ({ pics, onClick }) => {
-  return (
-    <ul className="ImageGallery">
-      {pics.map(pic => {
-        return <ImageGalleryItem url={pic.webformatURL} key={pic.id} onClick={onClick}/>;
-      })}
-    </ul>
-  );
-};
+class ImageGallery extends Component {
+  componentDidUpdate() {
+    const { height: cardHeight } = document
+      .querySelector('.ImageGallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 4,
+      behavior: 'smooth',
+    });
+  }
+
+  render() {
+    const { pics, onClick } = this.props;
+    return (
+      <ul className="ImageGallery">
+        {pics.map(pic => {
+          return (
+            <ImageGalleryItem
+              url={pic.webformatURL}
+              key={pic.id}
+              onClick={onClick}
+            />
+          );
+        })}
+      </ul>
+    );
+  }
+}
 
 export default ImageGallery;
